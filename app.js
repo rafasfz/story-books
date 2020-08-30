a3382119b2b6752f590384dedd33d2e9e50bd259
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-08-30 19:29:34 -0300

    Creates static folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const path = require('path')
 
 const connectDB = require('./config/db')
 const routes = require('./routes')
@@ -21,6 +22,9 @@ if (process.env.NODE_ENV == 'development') {
 // Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs' }))
 app.set('view engine', '.hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', routes)
