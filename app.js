8b8b42c0888d4e5c4ea5f1c1dc7b7fe681508ee8
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-09-01 03:07:28 -0300

    Adds a icon for edit the stories

--- app.js
+++ app.js
@@ -35,14 +35,15 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 // Handlebars
 app.engine('.hbs', exphbs({
     helpers: {
         formatDate,
         stripTags,
-        truncate
+        truncate,
+        editIcon
     },
     defaultLayout: 'main' ,
     extname: '.hbs' 
@@ -60,6 +61,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use((req, res, next) => {
+    res.locals.user = req.user || null
+    next()
+}) 
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
