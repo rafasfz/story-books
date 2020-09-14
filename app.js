bc51ac9b26af8180e323a9f15c3f210e867a3355
Rafael Silva Freire <31613570+rafasfz@users.noreply.github.com>
2020-09-14 09:35:14 -0300

    Changed port for 3333

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.use((req, res, next) => {
     return res.status(404).render('error/404')
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ort ${PORT}`))
